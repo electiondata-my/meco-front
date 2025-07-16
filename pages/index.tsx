0861be54946e027cb0364b1ac746465aae0182f4
Engineer @ ElectionDataMY <engineer@electiondata.my>
2025-07-16 18:22:27 +0800

    myarea map for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ const Home: Page = ({
   params,
   selection,
   seat,
-  boundaries,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
@@ -28,7 +27,7 @@ const Home: Page = ({
         voters_total={seat.voters_total}
         pyramid={seat.pyramid}
         barmeter={seat.barmeter}
-        boundaries={boundaries}
+        boundaries={seat.boundaries}
       />
     </>
   );
@@ -52,20 +51,6 @@ export const getStaticProps: GetStaticProps = withi18n(
         else return e.value.data;
       });
 
-      const boundaries = {
-        bounds: [102.20593, 2.07252, 102.34511, 2.27764],
-        center: [102.32552, 2.17508],
-        polygons: {
-          "2018": ["peninsular_2018_parlimen", ["P.138"]],
-          "2003": ["peninsular_2003_parlimen", ["P.138"]],
-          "1994": ["peninsular_1994_parlimen", ["P.123"]],
-          "1984": ["peninsular_1984_parlimen", ["P.113"]],
-          "1974": ["peninsular_1974_parlimen", ["P.098"]],
-          "1959": ["peninsular_1959_parlimen", ["P.086"]],
-          "1955": ["peninsular_1955_parlimen", ["P.005"]],
-        },
-      };
-
       return {
         notFound: false,
         props: {
@@ -76,7 +61,6 @@ export const getStaticProps: GetStaticProps = withi18n(
           params: { seat_name: slug, type: type },
           selection: dropdown.data,
           seat: seat,
-          boundaries,
         },
       };
     } catch (error: any) {
