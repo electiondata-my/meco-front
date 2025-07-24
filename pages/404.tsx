fef2036639f84afd557fdddd9a1645cd6fdd641e
Engineer @ ElectionDataMY <engineer@electiondata.my>
2025-07-24 20:53:06 +0800

    fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,6 +5,7 @@ import { withi18n } from "@lib/decorators";
 import { useTranslation } from "@hooks/useTranslation";
 import { Page } from "@lib/types";
 import { GetStaticProps } from "next";
+import SectionGrid from "@components/Section/section-grid";
 
 const Error404: Page = () => {
   const { t } = useTranslation("error");
@@ -12,13 +13,15 @@ const Error404: Page = () => {
     <>
       <Metadata title={t("404.title") as string} keywords={""} />
 
-      <Container className="min-h-[76vh] pt-7 text-zinc-900">
-        <ErrorStatus
-          title={t("404.title") as string}
-          description={t("404.description")}
-          code={404}
-          reason={t("404.reason")}
-        />
+      <Container className="min-h-[76vh] pt-7 text-txt-black-900">
+        <SectionGrid className="items-start justify-start">
+          <ErrorStatus
+            title={t("404.title") as string}
+            description={t("404.description")}
+            code={404}
+            reason={t("404.reason")}
+          />
+        </SectionGrid>
       </Container>
     </>
   );
